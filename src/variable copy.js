73428dc9988004b684f9a050607ccaf4e7e7d566
Siddharth Sinha <sid3345@gmail.com>
2021-07-05 21:30:53 +0530

    variable fields

---
+++ src/variable copy.js
@@ -0,0 +1,11 @@
+module.exports = {
+name : 'Aakash',
+position: 'Web Developer',
+linkedin : 'https://www.linkedin.com/in/aakash333',
+hackerearth: '',
+hackerrank: 'https://www.hackerrank.com/kevinoberoy98?hr_r=1',
+github: 'https://github.com/aks318',
+instagram: 'https://Instagram.com/aksthetic311',
+mail:'',
+image:'img/Aakash.jpg'
+}
